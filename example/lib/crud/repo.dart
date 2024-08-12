2bfb49ec9b05f5ee840353c0033cb52a0a770d48
Himanshu Rawat <himanshu.rawat@meragi.com>
2024-08-12 17:40:25 +0530

    feat: update repo name

--- example/lib/crud/repo.dart
+++ example/lib/crud/repo.dart
@@ -1,13 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_meragi_design/flutter_meragi_design.dart';
 
-class ExampleRepo extends CRUDRepository {
+class ExampleRepo extends MDRepository {
   Dio dio = Dio();
 
   @override
-  Future create(String url,
-      {Map<String, dynamic> data = const {},
-      Map<String, dynamic> files = const {}}) {
+  Future create(String url, {Map<String, dynamic> data = const {}, Map<String, dynamic> files = const {}}) {
     // TODO: implement create
     throw UnimplementedError();
   }
@@ -26,8 +24,7 @@ class ExampleRepo extends CRUDRepository {
 
   @override
   Future getList(String url,
-      {List<Map<String, String>> filters = const [],
-      List<Map<String, String>> sorters = const []}) async {
+      {List<Map<String, String>> filters = const [], List<Map<String, String>> sorters = const []}) async {
     Response res = await dio.get(url);
     if (res.statusCode == 200) {
       var rData = res.data;
@@ -36,8 +33,7 @@ class ExampleRepo extends CRUDRepository {
   }
 
   @override
-  Future getOne(String url, id,
-      {List<Map<String, String>> filters = const []}) async {
+  Future getOne(String url, id, {List<Map<String, String>> filters = const []}) async {
     Response res = await dio.get("$url/$id");
     if (res.statusCode == 200) {
       var rData = res.data;
