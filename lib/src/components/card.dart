317b4da42d75d16800072b27ec7c93fe26b24872
Mayank Kushal <mayank.kushal@meragi.com>
2024-08-20 06:12:26 +0530

    feat: extract cardDecoration from card class

--- lib/src/components/card.dart
+++ lib/src/components/card.dart
@@ -13,45 +13,45 @@ class MDCard extends StatelessWidget {
   final CardType type;
   final CardSize size;
   final CrossAxisAlignment alignment;
+  final CardDecoration? cardDecoration;
 
-  const MDCard(
-      {super.key,
-      this.header,
-      required this.body,
-      this.footer,
-      this.type = CardType.defaultType,
-      this.size = CardSize.rg,
-      this.alignment = CrossAxisAlignment.center});
+  const MDCard({
+    super.key,
+    this.header,
+    required this.body,
+    this.footer,
+    this.type = CardType.defaultType,
+    this.size = CardSize.rg,
+    this.alignment = CrossAxisAlignment.center,
+    this.cardDecoration,
+  });
 
   @override
   Widget build(BuildContext context) {
-    CardDecoration cardDecoration = CardDecoration(
+    CardDecoration finalCardDecoration = CardDecoration(
       context: context,
       type: type,
       size: size,
-    );
+    ).merge(cardDecoration);
+
     return Container(
       width: double.infinity,
-      padding: cardDecoration.padding,
-      decoration: CardDecoration(
-        context: context,
-        type: type,
-        size: size,
-      ).decoration,
+      padding: finalCardDecoration.padding,
+      decoration: finalCardDecoration.decoration,
       child: Column(
         crossAxisAlignment: alignment,
         children: [
           header ?? const SizedBox.shrink(),
           if (header != null)
             Divider(
-              height: cardDecoration.dividerHeight,
-              thickness: cardDecoration.dividerThickness,
+              height: finalCardDecoration.dividerHeight,
+              thickness: finalCardDecoration.dividerThickness,
             ),
           body,
           if (footer != null)
             Divider(
-              height: cardDecoration.dividerHeight,
-              thickness: cardDecoration.dividerThickness,
+              height: finalCardDecoration.dividerHeight,
+              thickness: finalCardDecoration.dividerThickness,
             ),
           footer ?? const SizedBox.shrink(),
         ],
@@ -65,15 +65,34 @@ class CardDecoration {
   final CardType type;
   final CardSize size;
 
-  CardDecoration(
-      {required this.context, required this.type, required this.size})
-      : assert(context != null, 'context cannot be null'),
-        assert(type != null, 'type cannot be null'),
-        assert(size != null, 'size cannot be null');
+  final EdgeInsets? paddingOverride;
+  final Color? backgroundColorOverride;
+  final Color? borderColorOverride;
+  final double? borderRadiusOverride;
+  final double? dividerHeightOverride;
+  final double? dividerThicknessOverride;
+  final double? borderWidthOverride;
+
+  CardDecoration({
+    required this.context,
+    required this.type,
+    required this.size,
+    this.paddingOverride,
+    this.backgroundColorOverride,
+    this.borderColorOverride,
+    this.borderRadiusOverride,
+    this.dividerHeightOverride,
+    this.dividerThicknessOverride,
+    this.borderWidthOverride,
+  });
 
   ThemeToken get token => MeragiTheme.of(context).token;
 
   EdgeInsets get padding {
+    if (paddingOverride != null) {
+      return paddingOverride!;
+    }
+
     switch (size) {
       case CardSize.sm:
         return token.smCardPadding;
@@ -88,7 +107,7 @@ class CardDecoration {
     return BoxDecoration(
       color: backgroundColor,
       border: Border.all(
-        width: 1,
+        width: borderWidth,
         color: borderColor,
       ),
       borderRadius: BorderRadius.circular(borderRadius),
@@ -96,6 +115,10 @@ class CardDecoration {
   }
 
   double get borderRadius {
+    if (borderRadiusOverride != null) {
+      return borderRadiusOverride!;
+    }
+
     switch (size) {
       case CardSize.sm:
         return token.smCardBorderRadius;
@@ -106,11 +129,32 @@ class CardDecoration {
     }
   }
 
-  double get dividerHeight => token.cardDividerHeight;
+  Color get backgroundColor {
+    if (backgroundColorOverride != null) {
+      return backgroundColorOverride!;
+    }
 
-  double get dividerThickness => token.cardDividerThickness;
+    switch (type) {
+      case CardType.primary:
+        return token.primaryCardBackgroundColor;
+      case CardType.secondary:
+        return token.secondaryCardBackgroundColor;
+      case CardType.danger:
+        return token.dangerCardBackgroundColor;
+      case CardType.success:
+        return token.successCardBackgroundColor;
+      case CardType.warning:
+        return token.warningCardBackgroundColor;
+      default:
+        return token.defaultCardBackgroundColor;
+    }
+  }
 
   Color get borderColor {
+    if (borderColorOverride != null) {
+      return borderColorOverride!;
+    }
+
     switch (type) {
       case CardType.primary:
         return token.primaryCardBorderColor;
@@ -127,20 +171,77 @@ class CardDecoration {
     }
   }
 
-  Color get backgroundColor {
-    switch (type) {
-      case CardType.primary:
-        return token.primaryCardBackgroundColor;
-      case CardType.secondary:
-        return token.secondaryCardBackgroundColor;
-      case CardType.danger:
-        return token.dangerCardBackgroundColor;
-      case CardType.success:
-        return token.successCardBackgroundColor;
-      case CardType.warning:
-        return token.warningCardBackgroundColor;
-      default:
-        return token.defaultCardBackgroundColor;
+  double get borderWidth {
+    if (borderWidthOverride != null) {
+      return borderWidthOverride!;
     }
+
+    return token.cardBorderWidth;
+  }
+
+  double get dividerHeight {
+    if (dividerHeightOverride != null) {
+      return dividerHeightOverride!;
+    }
+
+    return token.cardDividerHeight;
+  }
+
+  double get dividerThickness {
+    if (dividerThicknessOverride != null) {
+      return dividerThicknessOverride!;
+    }
+
+    return token.cardDividerThickness;
+  }
+
+  CardDecoration copyWith({
+    BuildContext? context,
+    CardType? type,
+    CardSize? size,
+    Color? backgroundColorOverride,
+    Color? borderColorOverride,
+    double? dividerHeightOverride,
+    double? dividerThicknessOverride,
+    double? borderWidthOverride,
+    EdgeInsets? paddingOverride,
+    double? borderRadiusOverride,
+  }) {
+    return CardDecoration(
+      context: context ?? this.context,
+      type: type ?? this.type,
+      size: size ?? this.size,
+      backgroundColorOverride:
+          backgroundColorOverride ?? this.backgroundColorOverride,
+      borderColorOverride: borderColorOverride ?? this.borderColorOverride,
+      dividerHeightOverride:
+          dividerHeightOverride ?? this.dividerHeightOverride,
+      dividerThicknessOverride:
+          dividerThicknessOverride ?? this.dividerThicknessOverride,
+      borderWidthOverride: borderWidthOverride ?? this.borderWidthOverride,
+      paddingOverride: paddingOverride ?? this.paddingOverride,
+      borderRadiusOverride: borderRadiusOverride ?? this.borderRadiusOverride,
+    );
+  }
+
+  CardDecoration merge(CardDecoration? other) {
+    if (other == null) {
+      return this;
+    }
+    return CardDecoration(
+      context: other.context,
+      type: other.type,
+      size: other.size,
+      backgroundColorOverride:
+          other.backgroundColorOverride ?? backgroundColorOverride,
+      borderColorOverride: other.borderColorOverride ?? borderColorOverride,
+      dividerHeightOverride:
+          other.dividerHeightOverride ?? dividerHeightOverride,
+      dividerThicknessOverride:
+          other.dividerThicknessOverride ?? dividerThicknessOverride,
+      borderWidthOverride: other.borderWidthOverride ?? borderWidthOverride,
+      paddingOverride: other.paddingOverride ?? paddingOverride,
+      borderRadiusOverride: other.borderRadiusOverride ?? borderRadiusOverride,
+    );
   }
 }
