9ed51e7ac3558041ad9b05e5537253521fdf0933
Himanshu Rawat <himanshu.rawat@meragi.com>
2024-08-27 19:27:38 +0530

    feat: add custom in getone

--- lib/src/crud/request/repo.dart
+++ lib/src/crud/request/repo.dart
@@ -7,5 +7,5 @@ abstract class MDRepository {
   Future<dynamic> create(String url, {Map<String, dynamic> data = const {}, Map<String, dynamic> files = const {}});
   Future<dynamic> update(String url, dynamic id, {Map<String, dynamic> data = const {}});
   Future<dynamic> delete(String url, dynamic id, {Map<String, dynamic>? params});
-  Future<dynamic> custom(dynamic id);
+  Future<dynamic> custom(String url, {List<Map<String, String>> filters = const []});
 }
