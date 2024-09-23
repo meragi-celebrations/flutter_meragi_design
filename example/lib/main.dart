685948970bfc18ceb7f9af16c2956ef3aa7c2d34
Mayank Kushal <mayank.kushal@meragi.com>
2024-09-23 18:53:55 +0530

    feat: add segmented button

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -268,6 +268,11 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
         Story(
+            name: "Button/Segmented",
+            builder: (context) {
+              return SegmentedButtonStory();
+            }),
+        Story(
           name: "Data/Description",
           builder: (context) {
             return MDDescription(
@@ -815,7 +820,7 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
         Story(
-            name: "Carousel",
+            name: "Data/Carousel",
             builder: (context) {
               return MDScaffold(
                 body: MDCarousel(
@@ -993,6 +998,51 @@ class _LayoutViewState extends State<LayoutView> {
         },
       ),
       content: pages[_currentIndex]['widget'],
+    );
+  }
+}
+
+class SegmentedButtonStory extends StatefulWidget {
+  const SegmentedButtonStory({super.key});
+
+  @override
+  State<SegmentedButtonStory> createState() => _SegmentedButtonStoryState();
+}
+
+enum Calendar { day, week, month, year }
+
+class _SegmentedButtonStoryState extends State<SegmentedButtonStory> {
+  Calendar calendarView = Calendar.day;
+  @override
+  Widget build(BuildContext context) {
+    return MDSegmentedButton<Calendar>(
+      segments: const <ButtonSegment<Calendar>>[
+        ButtonSegment<Calendar>(
+            value: Calendar.day,
+            label: Text('Day'),
+            icon: Icon(Icons.calendar_view_day)),
+        ButtonSegment<Calendar>(
+            value: Calendar.week,
+            label: Text('Week'),
+            icon: Icon(Icons.calendar_view_week)),
+        ButtonSegment<Calendar>(
+            value: Calendar.month,
+            label: Text('Month'),
+            icon: Icon(Icons.calendar_view_month)),
+        ButtonSegment<Calendar>(
+            value: Calendar.year,
+            label: Text('Year'),
+            icon: Icon(Icons.calendar_today)),
+      ],
+      selected: <Calendar>{calendarView},
+      onSelectionChanged: (Set<Calendar> newSelection) {
+        setState(() {
+          // By default there is only a single segment that can be
+          // selected at one time, so its value is always the first
+          // item in the selected set.
+          calendarView = newSelection.first;
+        });
+      },
     );
   }
 }
